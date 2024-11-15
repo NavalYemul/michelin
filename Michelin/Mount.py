b6c9143df276a5277bc957d02d3720b7de594fec
NavalYemul <107034512+NavalYemul@users.noreply.github.com>
2024-11-15 12:02:33 +0530

    Update Mount.py

--- Michelin/Mount.py
+++ Michelin/Mount.py
@@ -3,3 +3,9 @@ dbutils.fs.mount(
   source = "wasbs://<container>@<storage_account>.blob.core.windows.net",
   mount_point = "/mnt/<storage_account>/<container>",
   extra_configs = {"fs.azure.account.key.<storage_account>.blob.core.windows.net":"<Access Key>"})
+
+#this is the final mounting
+dbutils.fs.mount(
+  source = "wasbs://raw@adls.blob.core.windows.net",
+  mount_point = "/mnt/<storage_account>/<container>",
+  extra_configs = {"fs.azure.account.key.<storage_account>.blob.core.windows.net":"<Access Key>"})
